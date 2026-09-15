bb89c475037d4bdb99b19385eb2a0c282106d20a
Kapil <yadavkapil8319@gmail.com>
2026-09-15 13:41:25 +0530

    Configure database URL

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:kapil%401234@localhost:5432/student"
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:kapil%401234@localhost:5432/student")
 
 engine = create_engine(DATABASE_URL)
 
